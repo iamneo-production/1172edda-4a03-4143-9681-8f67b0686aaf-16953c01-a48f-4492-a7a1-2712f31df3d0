3d7e3d0e5dffc957e3288ba58c1c2d985ba63149
hemayyanar <ayyanar.kaliyappan@iamneo.ai>
2023-09-19 11:21:50 +0000

    Backup Branch before deleting

---
+++ angular/index.py
@@ -0,0 +1,225 @@
+
+for i in range(100):
+    print("Fdasfdas")
+
+
+
+
+
+
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
+for i in range(100):
+    print("Fdasfdas")
